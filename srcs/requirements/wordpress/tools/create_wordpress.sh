fc7f57a097cd9ddf4eedd934f949aeb1e9f41c63
Victor Héran <leroierrant@MacBook-Pro-de-Victor.local>
2023-01-25 10:28:01 +0100

    Upload by leroierrant 2023-01-25_10:28:01

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -9,7 +9,7 @@ else
 	wget https://wordpress.org/latest.tar.gz
 	tar -xzvf latest.tar.gz
 	#mv /var/www/html/wordpress/* /var/www/html
-	#mv wordpress/* .
+	mv wordpress/* .
 	rm -rf latest.tar.gz
 	rm -rf ./wordpress
 	ls -l >> ./SHOW_ME.txt
@@ -23,10 +23,12 @@ else
 
 
 	#Inport env variables in the config file
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	sed -i "s/username_here/$WORDPRESS_DB_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" wp-config-sample.php
+	sed -i "s/localhost/$WORDPRESS_DB_HOST/g" wp-config-sample.php
+	sed -i "s/database_name_here/$WORDPRESS_DB_DATABASE/g" wp-config-sample.php
+	sed -i '87 s/.*/'"define( 'WP_INSTALLING', true );"'/' wp-config-sample.php
+	echo "define( 'WP_HOME', 'http://example.com' );" >> wp-config-sample.php
 	mv wp-config-sample.php wp-config.php
 fi
 
